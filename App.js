1f0c12183b94dafdacec6052fe0ccb28df4fc9bc
Daniel Saputra <danielwetan.io@gmail.com>
2020-07-30 09:07:05 +0700

    feature: Improve bottom nav ui

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {View, Text} from 'react-native';
 
@@ -10,6 +11,10 @@ import Chats from './src/screens/Chats';
 import Contacts from './src/screens/Contacts';
 import Profile from './src/screens/Profile';
 import Maps from './src/screens/Maps';
+
+// import FontAwesome from 'react-native-vector-icons/FontAwesome5';
+import FontAwesome from 'react-native-vector-icons/FontAwesome5';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -55,11 +60,59 @@ const App = () => {
       <Provider store={store}>
         <PersistGate persistor={persistor}>
           <NavigationContainer>
-            <Tab.Navigator>
-              <Tab.Screen name="Chats" component={ChatsStackScreen} />
-              <Tab.Screen name="Contacts" component={ContactsStackScreen} />
-              <Tab.Screen name="Maps" component={MapsStackScreen} />
-              <Tab.Screen name="Profile" component={ProfileStackScreen} />
+            <Tab.Navigator
+              tabBarOptions={{
+                labelStyle: {
+                  fontSize: 12,
+                  fontFamily: 'Quicksand-Bold',
+                  // marginBottom: 15,
+                  paddingBottom: 15,
+                },
+                style: {
+                  height: 80,
+                },
+                tabStyle: {
+                  marginTop: 15,
+                },
+              }}>
+              <Tab.Screen
+                options={{
+                  tabBarLabel: 'Profile',
+
+                  tabBarIcon: ({color, size}) => (
+                    <FontAwesome name="comment" color={color} size={30} />
+                  ),
+                }}
+                name="Chats"
+                component={ChatsStackScreen}
+              />
+              <Tab.Screen
+                options={{
+                  tabBarIcon: ({color, size}) => (
+                    <FontAwesome name="address-book" color={color} size={30} />
+                  ),
+                }}
+                name="Contacts"
+                component={ContactsStackScreen}
+              />
+              <Tab.Screen
+                options={{
+                  tabBarIcon: ({color, size}) => (
+                    <FontAwesome name="compass" color={color} size={30} />
+                  ),
+                }}
+                name="Maps"
+                component={MapsStackScreen}
+              />
+              <Tab.Screen
+                options={{
+                  tabBarIcon: ({color, size}) => (
+                    <FontAwesome name="user" color={color} size={30} />
+                  ),
+                }}
+                name="Profile"
+                component={ProfileStackScreen}
+              />
             </Tab.Navigator>
           </NavigationContainer>
         </PersistGate>
